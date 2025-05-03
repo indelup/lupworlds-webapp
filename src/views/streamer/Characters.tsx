4342b63e390a39e70dfcafbef832b36e7b13bd73
Melda <david@fml.cl>
2025-05-03 13:24:31 +0200

    Added component library and initial character cards

--- src/views/streamer/Characters.tsx
+++ src/views/streamer/Characters.tsx
@@ -1,4 +1,8 @@
 import { Character } from "src/types";
+import classes from "./Characters.module.scss";
+import Tilt from "react-parallax-tilt";
+import { Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 export const Characters = () => {
     // TODO - This should fetch characters from the API
@@ -12,11 +16,62 @@ export const Characters = () => {
             backImage: "path2",
             rarity: 5,
         },
+        {
+            id: "testCharacter",
+            worldId: "Indelup",
+            name: "Vani",
+            description: "A",
+            frontImage: "path1",
+            backImage: "path2",
+            rarity: 5,
+        },
+        {
+            id: "testCharacter",
+            worldId: "Indelup",
+            name: "Afie",
+            description: "A",
+            frontImage: "path1",
+            backImage: "path2",
+            rarity: 5,
+        },
+        {
+            id: "testCharacter",
+            worldId: "Indelup",
+            name: "Otro",
+            description: "A",
+            frontImage: "path1",
+            backImage: "path2",
+            rarity: 5,
+        },
     ];
 
     // TODO: DISPLAY CHARACTERS
     // TODO: ADD CREATE CHARACTER BUTTON
     // TODO: USE API INSTEAD OF TEST DATA
 
-    return <div>Vista de Personajes</div>;
+    return (
+        <>
+            <div className={classes.actions}>
+                <Button icon={<PlusOutlined />} color="cyan" variant="solid">
+                    New Character
+                </Button>
+            </div>
+            <div className={classes.cardList}>
+                {characters.map((character) => {
+                    return (
+                        <Tilt
+                            tiltReverse
+                            glareEnable
+                            glareColor="white"
+                            glarePosition="all"
+                            glareBorderRadius="0.5rem"
+                            glareMaxOpacity={0.2}
+                        >
+                            <div className={classes.card}>{character.name}</div>
+                        </Tilt>
+                    );
+                })}
+            </div>
+        </>
+    );
 };
--- src/views/streamer/StreamerApp.tsx
+++ src/views/streamer/StreamerApp.tsx
@@ -1,61 +1,88 @@
 import { useStore, AppState } from "src/hooks/useStore";
-import classes from "./StreamerApp.module.scss";
 import { useCheckUser } from "src/hooks/useCheckUser";
 import { useState } from "react";
 import { Characters } from "./Characters";
 import { Materials } from "./Materials";
 import { Banners } from "./Banners";
 import { Stores } from "./Stores";
+import classes from "./StreamerApp.module.scss";
+
+import type { MenuProps } from "antd";
+import { Layout, Menu } from "antd";
+import {
+    RadarChartOutlined,
+    GoldOutlined,
+    FireOutlined,
+    ShoppingCartOutlined,
+    TwitchOutlined,
+    SettingOutlined,
+} from "@ant-design/icons";
+
+const { Header, Content, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const getItem = (
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+): MenuItem => {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    } as MenuItem;
+};
+
+const items: MenuItem[] = [
+    getItem("Characters", "characters", <RadarChartOutlined />),
+    getItem("Materials", "materials", <GoldOutlined />),
+    getItem("Banners", "banners", <FireOutlined />),
+    getItem("Stores", "stores", <ShoppingCartOutlined />),
+    getItem("Twitch Config", "twitch", <TwitchOutlined />),
+    getItem("Settings", "settings", <SettingOutlined />),
+];
 
 export const StreamerApp = () => {
     const user = useStore((state: AppState) => state.user);
     const [view, setView] = useState<string>("characters");
+    const [collapsed, setCollapsed] = useState<boolean>(false);
 
     const ActiveView = getActiveView(view);
-
-    const changeActiveView = (newView: string) => {
-        setView(newView);
-    };
 
     useCheckUser();
 
     return (
-        <div className={classes.background}>
-            <div className={classes.sidebar}>
-                <div className={classes.logo}>LOGO ACA</div>
-                <div
-                    className={classes.sidebarButton}
-                    onClick={() => changeActiveView("characters")}
-                >
-                    PERSONAJES
+        <Layout className={classes.layout}>
+            <Sider
+                collapsible
+                collapsed={collapsed}
+                onCollapse={(value) => setCollapsed(value)}
+            >
+                <div className={classes.logoContainer}>
+                    <img className={classes.logo} src="/indelup_iso.png" />
                 </div>
-                <div
-                    className={classes.sidebarButton}
-                    onClick={() => changeActiveView("materials")}
-                >
-                    MATERIALES
-                </div>
-                <div
-                    className={classes.sidebarButton}
-                    onClick={() => changeActiveView("banners")}
-                >
-                    BANNERS
-                </div>
-                <div
-                    className={classes.sidebarButton}
-                    onClick={() => changeActiveView("stores")}
-                >
-                    TIENDAS
-                </div>
-            </div>
-            <div className={classes.contentContainer}>
-                <div className={classes.contentHeader}>
-                    <div>Hola {user?.alias}</div>
-                    <div>ESTA ES LA VISTA DE STREAMER</div>
-                </div>
-                <ActiveView />
-            </div>
-        </div>
+                <Menu
+                    theme="dark"
+                    selectedKeys={[view]}
+                    mode="inline"
+                    items={items}
+                    onClick={(e) => {
+                        setView(e.key);
+                    }}
+                />
+            </Sider>
+            <Layout>
+                <Header />
+                <Content className={classes.background}>
+                    <div className={classes.content}>
+                        <ActiveView />
+                    </div>
+                </Content>
+            </Layout>
+        </Layout>
     );
 };
 
